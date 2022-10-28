b24a8b65e2ff4f4bc3c974d7dcbaf476ca8b88c9
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-10-28 12:45:36 +0800

    del gym; embed hyper parameters

--- stragety/DQN.py
+++ stragety/DQN.py
@@ -1,5 +1,4 @@
-from typing import List, Callable
-import random
+from typing import List
 """
 Reference: https://mofanpy.com/tutorials/machine-learning/torch/DQN
 """
@@ -7,35 +6,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-import gym
 
 if __name__=="__main__":
     from __init__ import single_host
 else:
     from stragety import single_host
 
-# Hyper Parameters
-BATCH_SIZE = 32
-LR = 0.01                   # learning rate
-EPSILON = 0.9               # greedy policy
-GAMMA = 0.9                 # reward discount
-TARGET_REPLACE_ITER = 100   # target update frequency
-MEMORY_CAPACITY = 2000
-if __name__=='__main__':
-    env = gym.make('CartPole-v1', render_mode="human")
-    env = env.unwrapped
-    N_ACTIONS = env.action_space.n
-    N_STATES = env.observation_space.shape[0]
-    ENV_A_SHAPE = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape     # to confirm the shape
-else:
-    env:any
-    N_ACTIONS = 20
-    N_STATES = 20*25 + 15
-    ENV_A_SHAPE = 0
-
-
 class Net(nn.Module):
-    def __init__(self, ):
+    def __init__(self, 
+        N_ACTIONS,
+        N_STATES
+    ):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(N_STATES, 50)
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
@@ -48,10 +29,29 @@ class Net(nn.Module):
         actions_value = self.out(x)
         return actions_value
 
-
 class DQN(object):
-    def __init__(self):
-        self.eval_net, self.target_net = Net(), Net()
+    def __init__(self,
+        BATCH_SIZE = 32,
+        LR = 0.01,                  # learning rate
+        EPSILON = 0.9,              # greedy policy
+        GAMMA = 0.9,                # reward discount
+        TARGET_REPLACE_ITER = 100,   # target update frequency
+        MEMORY_CAPACITY = 2000,
+        N_ACTIONS = 20,
+        N_STATES = 20*25 + 15,
+        ENV_A_SHAPE = 0
+    ):
+        self.BATCH_SIZE=BATCH_SIZE
+        self.LR=LR
+        self.EPSILON=EPSILON
+        self.GAMMA=GAMMA
+        self.TARGET_REPLACE_ITER=TARGET_REPLACE_ITER
+        self.MEMORY_CAPACITY=MEMORY_CAPACITY
+        self.N_ACTIONS=N_ACTIONS
+        self.N_STATES=N_STATES
+        self.ENV_A_SHAPE=ENV_A_SHAPE
+
+        self.eval_net, self.target_net = Net(self.N_ACTIONS,self.N_STATES), Net(self.N_ACTIONS,self.N_STATES)
 
         self.learn_step_counter = 0                                     # for target updating
         self.memory_counter = 0                                         # for storing memory
@@ -62,40 +62,40 @@ class DQN(object):
     def choose_action(self, x):
         x = torch.unsqueeze(torch.FloatTensor(x), 0)
         # input only one sample
-        if np.random.uniform() < EPSILON:   # greedy
+        if np.random.uniform() < self.EPSILON:   # greedy
             actions_value = self.eval_net.forward(x)
             action = torch.max(actions_value, 1)[1].data.numpy()
-            action = action[0] if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)  # return the argmax index
+            action = action[0] if self.ENV_A_SHAPE == 0 else action.reshape(self.ENV_A_SHAPE)  # return the argmax index
         else:   # random
-            action = np.random.randint(0, N_ACTIONS)
-            action = action if ENV_A_SHAPE == 0 else action.reshape(ENV_A_SHAPE)
+            action = np.random.randint(0, self.N_ACTIONS)
+            action = action if self.ENV_A_SHAPE == 0 else action.reshape(self.ENV_A_SHAPE)
         return action
 
     def store_transition(self, s, a, r, s_):
         transition = np.hstack((s, [a, r], s_))
         # replace the old memory with new memory
-        index = self.memory_counter % MEMORY_CAPACITY
+        index = self.memory_counter % self.MEMORY_CAPACITY
         self.memory[index, :] = transition
         self.memory_counter += 1
 
     def learn(self):
         # target parameter update
-        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
+        if self.learn_step_counter % self.TARGET_REPLACE_ITER == 0:
             self.target_net.load_state_dict(self.eval_net.state_dict())
         self.learn_step_counter += 1
 
         # sample batch transitions
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
+        sample_index = np.random.choice(self.MEMORY_CAPACITY, self.BATCH_SIZE)
         b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
-        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
+        b_s = torch.FloatTensor(b_memory[:, :self.N_STATES])
+        b_a = torch.LongTensor(b_memory[:, self.N_STATES:self.N_STATES+1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, self.N_STATES+1:self.N_STATES+2])
+        b_s_ = torch.FloatTensor(b_memory[:, -self.N_STATES:])
 
         # q_eval w.r.t the action in experience
         q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
         q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
-        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
+        q_target = b_r + self.GAMMA * q_next.max(1)[0].view(self.BATCH_SIZE, 1)   # shape (batch, 1)
         loss = self.loss_func(q_eval, q_target)
 
         self.optimizer.zero_grad()
@@ -149,39 +149,5 @@ class DQN_Manager:
         self.multi_hosts_s = self.multi_hosts_new_s
         self.multi_hosts_new_s = None
 
-        if self.dqn.memory_counter > MEMORY_CAPACITY:
+        if self.dqn.memory_counter > self.dqn.MEMORY_CAPACITY:
             self.dqn.learn()
-
-
-if __name__=="__main__":
-    dqn = DQN()
-
-    print('\nCollecting experience...')
-    for i_episode in range(400):
-        s = env.reset()[0]
-        ep_r = 0
-        while True:
-            env.render()
-            a = dqn.choose_action(s)
-
-            # take action
-            s_, r, done, _, _ = env.step(a)
-
-            # modify the reward
-            x, x_dot, theta, theta_dot = s_
-            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            r = r1 + r2
-
-            dqn.store_transition(s, a, r, s_)
-
-            ep_r += r
-            if dqn.memory_counter > MEMORY_CAPACITY:
-                dqn.learn()
-                if done:
-                    print('Ep: ', i_episode,
-                        '| Ep_r: ', round(ep_r, 2))
-
-            if done:
-                break
-            s = s_
