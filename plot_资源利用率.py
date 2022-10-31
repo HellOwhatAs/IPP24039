1c93890a57f1ea779690338e3f6c125c573a2dbd
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-10-31 20:49:41 +0800

    nop

---
+++ plot_资源利用率.py
@@ -0,0 +1,60 @@
+import random, numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from frame import Frame
+from collections import defaultdict
+seed=0
+lmd=0.6
+jobnum=20000
+
+# main
+from stragety import single_host,multi_hosts
+title="main stragety"
+random.seed(seed),np.random.seed(seed)
+frame=Frame(single_host,multi_hosts,lmd=lmd)
+r=[]
+all_tasks_size=defaultdict(float)
+for i in tqdm(range(jobnum),title):
+    frame.step()
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+    r.append(sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts)))
+plt.title("utilization rate")
+plt.plot(r,label=title)
+plt.legend()
+
+# random
+from stragety.Random import single_host,multi_hosts
+title="random stragety"
+random.seed(seed),np.random.seed(seed)
+frame=Frame(single_host,multi_hosts,lmd=lmd)
+r=[]
+all_tasks_size=defaultdict(float)
+for i in tqdm(range(jobnum),title):
+    frame.step()
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+    r.append(sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts)))
+
+plt.plot(r,label=title)
+plt.legend()
+
+# greedy
+from stragety.Greedy import single_host,multi_hosts
+title="greedy stragety"
+random.seed(seed),np.random.seed(seed)
+frame=Frame(single_host,multi_hosts,lmd=lmd)
+r=[]
+all_tasks_size=defaultdict(float)
+for i in tqdm(range(jobnum),title):
+    frame.step()
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+    r.append(sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts)))
+
+plt.plot(r,label=title)
+plt.legend()
+
+
+
+plt.show()
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random, numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from frame import Frame
-
+from collections import defaultdict
 seed=0
 lmd=0.6
 jobnum=20000
@@ -13,6 +13,7 @@ title="main stragety"
 random.seed(seed),np.random.seed(seed)
 frame=Frame(single_host,multi_hosts,lmd=lmd)
 y1,y2=[],[]
+all_tasks_size=defaultdict(float)
 for i in tqdm(range(jobnum),title):
     frame.step()
     y1.append(frame.step_start_line-frame.step_j.arrive_time)
@@ -20,10 +21,16 @@ for i in tqdm(range(jobnum),title):
         frame.cores_on_hosts[frame.step_host][c] 
         for c in frame.step_assignment
     )-frame.step_start_line)
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+
 plt.figure()
 plt.title(title)
 plt.plot(y1,label="wait time")
 plt.plot(y2,label="execute time")
+print(
+    sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts))
+)
 plt.legend()
 
 # random
@@ -32,6 +39,7 @@ title="random stragety"
 random.seed(seed),np.random.seed(seed)
 frame=Frame(single_host,multi_hosts,lmd=lmd)
 y1,y2=[],[]
+all_tasks_size=defaultdict(float)
 for i in tqdm(range(jobnum),title):
     frame.step()
     y1.append(frame.step_start_line-frame.step_j.arrive_time)
@@ -39,10 +47,16 @@ for i in tqdm(range(jobnum),title):
         frame.cores_on_hosts[frame.step_host][c] 
         for c in frame.step_assignment
     )-frame.step_start_line)
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+
 plt.figure()
 plt.title(title)
 plt.plot(y1,label="wait time")
 plt.plot(y2,label="execute time")
+print(
+    sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts))
+)
 plt.legend()
 
 # greedy
@@ -51,6 +65,7 @@ title="greedy stragety"
 random.seed(seed),np.random.seed(seed)
 frame=Frame(single_host,multi_hosts,lmd=lmd)
 y1,y2=[],[]
+all_tasks_size=defaultdict(float)
 for i in tqdm(range(jobnum),title):
     frame.step()
     y1.append(frame.step_start_line-frame.step_j.arrive_time)
@@ -58,10 +73,16 @@ for i in tqdm(range(jobnum),title):
         frame.cores_on_hosts[frame.step_host][c] 
         for c in frame.step_assignment
     )-frame.step_start_line)
+
+    all_tasks_size[frame.step_host]+=sum(j.size for j in frame.step_j.tasks)
+
 plt.figure()
 plt.title(title)
 plt.plot(y1,label="wait time")
 plt.plot(y2,label="execute time")
+print(
+    sum(all_tasks_size.values())/(frame.num_of_cores*frame.num_of_hosts*max(max(i) for i in frame.cores_on_hosts))
+)
 plt.legend()
 
 
